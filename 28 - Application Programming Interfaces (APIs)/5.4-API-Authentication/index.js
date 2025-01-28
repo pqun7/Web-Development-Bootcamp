8c00b29aafe9e648b191b067acd2ce92e8e93910
Ali <alinazer30@gmail.com>
2025-01-28 03:43:41 +0200

    Completed Leeson API Authentication

--- 28 - Application Programming Interfaces (APIs)/5.4-API-Authentication/index.js
+++ 28 - Application Programming Interfaces (APIs)/5.4-API-Authentication/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json, query, response } from "express";
 import axios from "axios";
 
 const app = express();
@@ -6,23 +6,55 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com/";
 
 //TODO 1: Fill in your values for the 3 types of auth.
-const yourUsername = "";
-const yourPassword = "";
-const yourAPIKey = "";
-const yourBearerToken = "";
+const yourUsername = "pqun";
+const yourPassword = "1450";
+const yourAPIKey = "4e78869a-dfbe-415d-bfac-6731371a935f";
+const yourBearerToken = "dd19a160-2c93-46b6-95a0-333748094c16";
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
 });
 
-app.get("/noAuth", (req, res) => {
-  //TODO 2: Use axios to hit up the /random endpoint
-  //The data you get back should be sent to the ejs file as "content"
-  //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
+app.get("/noAuth", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://secrets-api.appbrewery.com/random"
+    );
+    const result = JSON.stringify(response.data);
+    res.render("index.ejs", { content: result });
+
+    console.log(result);
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
-app.get("/basicAuth", (req, res) => {
+app.get("/basicAuth", async (req, res) => {
   //TODO 3: Write your code here to hit up the /all endpoint
+  try {
+    const response = await axios.get(
+      "https://secrets-api.appbrewery.com/all?page=2",
+      {
+        auth: {
+          username: yourUsername,
+          password: yourPassword,
+        },
+      }
+    );
+
+    console.log("GET Response:", response.data);
+    const result = JSON.stringify(response.data);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+
   //Specify that you only want the secrets from page 2
   //HINT: This is how you can use axios to do basic auth:
   // https://stackoverflow.com/a/74632908
@@ -36,15 +68,47 @@ app.get("/basicAuth", (req, res) => {
   */
 });
 
-app.get("/apiKey", (req, res) => {
+app.get("/apiKey", async (req, res) => {
   //TODO 4: Write your code here to hit up the /filter endpoint
+  try {
+    const response = await axios.get("https://secrets-api.appbrewery.com/filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey
+      },
+    });
+    const result = JSON.stringify(response.data);
+    console.log(response.data);
+    res.render("index.ejs", {content: result})
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
   //Filter for all secrets with an embarassment score of 5 or greater
   //HINT: You need to provide a query parameter of apiKey in the request.
 });
 
-app.get("/bearerToken", (req, res) => {
+app.get("/bearerToken", async (req, res) => {
   //TODO 5: Write your code here to hit up the /secrets/{id} endpoint
   //and get the secret with id of 42
+  try{
+    const response = await axios.get("https://secrets-api.appbrewery.com/secrets/42",{
+      headers:{
+        "Authorization" : `Bearer ${yourBearerToken}`
+      }
+    })
+    const result = JSON.stringify(response.data);
+    console.log(response.data);
+    res.render("index.ejs", {content: result});
+
+  }catch(error){
+    console.error("Failed to make request: ", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
   //HINT: This is how you can use axios to do bearer token auth:
   // https://stackoverflow.com/a/52645402
   /*
